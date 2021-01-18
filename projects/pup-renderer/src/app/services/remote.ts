2bc2a6b687b15468e008691382d1892cbb420bf5
Benjamin Dobler <benz@richapps.de>
2021-01-18 22:39:05 +0100

    feat: update/remove targets

--- projects/pup-renderer/src/app/services/remote.ts
+++ projects/pup-renderer/src/app/services/remote.ts
@@ -1,4 +1,5 @@
 import { NgZone } from '@angular/core';
+import { Browser } from 'puppeteer';
 import * as puppeteer from 'puppeteer-core';
 import { Target } from './target';
 
@@ -16,29 +17,43 @@ export class Remote {
 
     console.log(this.info);
 
-    const browser = await puppeteer.connect({
+    const browser: Browser = await puppeteer.connect({
       browserWSEndpoint: this.info.webSocketDebuggerUrl,
       defaultViewport: null,
     });
 
     const targets = await browser.targets();
 
-    targets.forEach((target) => {
+    const addTarget = (target) => {
       if (target.type() === 'page' || target.type() === 'webview') {
         const t = new Target(browser, this.zone);
-        t.original = target;
-        this.targets.push(t);
-      }
-    });
-
-    browser.on('targetcreated', (target) => {
-      if (target.type() === 'page' || target.type() === 'webview') {
-        const t = new Target(browser, this.zone);
+        t.url = target.url();
         t.original = target;
         this.zone.run(() => {
           this.targets.push(t);
         });
       }
+    };
+
+    targets.forEach((target) => {
+      addTarget(target);
+    });
+
+    browser.on('targetcreated', (target) => {
+      addTarget(target);
+    });
+
+    browser.on('targetdestroyed', (target) => {
+      this.zone.run(() => {
+        this.targets = this.targets.filter((t) => t.original !== target);
+      });
+    });
+
+    browser.on('targetchanged', (target) => {
+      const t = this.targets.find((t) => t.original === target);
+      this.zone.run(() => {
+        t.url = target.url();
+      });
     });
   }
 }
--- projects/pup-renderer/src/app/services/target.ts
+++ projects/pup-renderer/src/app/services/target.ts
@@ -1,32 +1,25 @@
-import { NgZone } from "@angular/core";
-import { Browser } from "puppeteer";
-import { Playback } from "./playback";
-import { Recording } from "./recording";
-
-
+import { NgZone } from '@angular/core';
+import { Browser } from 'puppeteer';
+import { Playback } from './playback';
+import { Recording } from './recording';
 
 export class Target {
+  url: string;
+  original: any;
+  recordings: Recording[] = [];
+  recording = false;
+  currentRecording: Recording;
 
-    original: any;
-    recordings: Recording[] = [];
+  constructor(private browser: Browser, private zone: NgZone) {}
 
-    recording = false;
+  startRecording() {
+    const recording = new Recording(this.original, this.zone);
+    this.currentRecording = recording;
+    recording.start();
+  }
 
-    currentRecording: Recording;
-
-    constructor(private browser: Browser, private zone: NgZone) {
-
-    }
-
-    startRecording() {
-        const recording = new Recording(this.original, this.zone);
-        this.currentRecording = recording;
-        recording.start();
-    }
-
-    startPlayback(recording: Recording) {
-        const playback = new Playback(recording.events, this.browser);
-        playback.start();
-    }
-
+  startPlayback(recording: Recording) {
+    const playback = new Playback(recording.events, this.browser);
+    playback.start();
+  }
 }
